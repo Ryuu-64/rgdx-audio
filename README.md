efd15513dd991ccf24faabbf6d26b23cc9801249
DESKTOP-FQ830JM\Ryuu <2357622935@qq.com>
2023-03-11 21:28:18 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# rgdx-sound
+#rgdx-audio
+
+[![](https://img.shields.io/badge/JDK-8+-green.svg)](https://www.oracle.com/java/technologies/javase/javase-jdk8-downloads.html)
+
+How to use?
+
+```java
+ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+	1, 1, 1, TimeUnit.MILLISECONDS,
+	new ArrayBlockingQueue<>(32),
+	new ThreadPoolExecutor.DiscardOldestPolicy()
+);
+SoundExecutor soundManager = new SoundExecutor(threadPoolExecutor);
+```
+
---
+++ src/main/java/org/ryuu/gdx/audio/SoundExecutor.java
@@ -0,0 +1,85 @@
+package org.ryuu.gdx.audio;
+
+import com.badlogic.gdx.audio.Sound;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+@AllArgsConstructor
+public class SoundExecutor {
+    private final Executor executor;
+
+    public SoundWrapper play(Sound sound) {
+        SoundWrapper soundWrapper = new SoundWrapper();
+        soundWrapper.sound = sound;
+        executor.execute(() -> {
+            long soundId = sound.play();
+            soundWrapper.soundId.set(soundId);
+        });
+        return soundWrapper;
+    }
+
+    public SoundWrapper play(Sound sound, float volume) {
+        SoundWrapper soundWrapper = new SoundWrapper();
+        soundWrapper.sound = sound;
+        executor.execute(() -> {
+            long soundId = sound.play(volume);
+            soundWrapper.soundId.set(soundId);
+        });
+        return soundWrapper;
+    }
+
+    public SoundWrapper play(Sound sound, float volume, float pitch, float pan) {
+        SoundWrapper soundWrapper = new SoundWrapper();
+        soundWrapper.sound = sound;
+        executor.execute(() -> {
+            long soundId = sound.play(volume, pitch, pan);
+            soundWrapper.soundId.set(soundId);
+        });
+        return soundWrapper;
+    }
+
+    public SoundWrapper loop(Sound sound) {
+        SoundWrapper soundWrapper = new SoundWrapper();
+        soundWrapper.sound = sound;
+        executor.execute(() -> {
+            long soundId = sound.loop();
+            soundWrapper.soundId.set(soundId);
+        });
+        return soundWrapper;
+    }
+
+    public SoundWrapper loop(Sound sound, float volume) {
+        SoundWrapper soundWrapper = new SoundWrapper();
+        soundWrapper.sound = sound;
+        executor.execute(() -> {
+            long soundId = sound.loop(volume);
+            soundWrapper.soundId.set(soundId);
+        });
+        return soundWrapper;
+    }
+
+    public SoundWrapper loop(Sound sound, float volume, float pitch, float pan) {
+        SoundWrapper soundWrapper = new SoundWrapper();
+        soundWrapper.sound = sound;
+        executor.execute(() -> {
+            long soundId = sound.loop(volume, pitch, pan);
+            soundWrapper.soundId.set(soundId);
+        });
+        return soundWrapper;
+    }
+
+    @ToString
+    public static class SoundWrapper {
+        @Getter
+        private final AtomicLong soundId = new AtomicLong();
+        @Getter
+        private volatile Sound sound;
+    }
+}
